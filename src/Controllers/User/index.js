a84ccf82109867788b7005ebb15f43df1acd9776
danilo <d.morya@speedflow.com>
2019-11-11 23:28:12 +0200

    catch 404 and 500 errors

--- src/Controllers/User/index.js
+++ src/Controllers/User/index.js
@@ -1,8 +1,8 @@
 import User from '../../Models/User';
 
 const getUser = async (req, res) => {
-  const response = await User.query().findById({});
-  return res.send(response);
+  //const response = await User.query().findById();
+  return res.send();
 };
 
 export default {
--- src/index.js
+++ src/index.js
@@ -23,5 +23,22 @@ dotenv.config();
 
   app.use(router);
 
+  app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    console.log(err.stack);
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+
   app.listen(process.env.PORT, () => console.log(`Running at ${process.env.PORT} port...`));
 })();
