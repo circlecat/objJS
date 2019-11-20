99dd410aa8265394a02bb8f250f09fcc72a67b9e
d.morya <d.morya@speedflow.com>
2019-11-20 16:54:33 +0200

    fix migration

--- db/migrations/20191120154447_create_folder_table.js
+++ db/migrations/20191120154447_create_folder_table.js
@@ -5,11 +5,7 @@ exports.up = knex =>
     table.integer('userId').unsigned().notNullable();
     table.foreign('userId').references('user.id').onDelete('CASCADE').onUpdate('CASCADE');
     table.string('title', 255).notNull();
-<<<<<<< HEAD:db/migrations/20191120154447_create_folder_table.js
     table.string('description', 3000).nullable();
-=======
-    table.string('body', 10000).notNull();
->>>>>>> e492e3dec77885cca510aa60cccc69bd770dd331:db/migrations/20191113144817_create_post_table.js
     table.timestamps(true, true);
   });
 
